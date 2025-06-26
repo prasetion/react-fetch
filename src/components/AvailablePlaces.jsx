9e07bd5311cd1d7354b1800e70c3b66e2fafb221
prasetio <prasetion@gmail.com>
2025-06-26 10:33:36 +0700

    local storage added

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,9 @@
 import Places from './Places.jsx';
+import {useState} from "react";
+const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
   return (
     <Places
       title="Available Places"
