a91774f1630a1694e87ca7738a1f3b86802116b5
prasetion <prasetion@gmail.com>
2025-06-27 21:34:48 +0700

    fetch in react using useEffect

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,15 +1,25 @@
 import Places from './Places.jsx';
-import {useState} from "react";
-const places = localStorage.getItem('places');
+import {useEffect, useState} from "react";
+// const places = localStorage.getItem('places');
 
-export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([])
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+export default function AvailablePlaces({onSelectPlace}) {
+    const [availablePlaces, setAvailablePlaces] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/places').then((res) => {
+                return res.json();
+            }
+        ).then((data) => {
+            setAvailablePlaces(data.places);
+        });
+    }, []);
+
+    return (
+        <Places
+            title="Available Places"
+            places={availablePlaces}
+            fallbackText="No places available."
+            onSelectPlace={onSelectPlace}
+        />
+    );
 }
