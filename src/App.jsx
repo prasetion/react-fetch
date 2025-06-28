67b163d5c59c959a61629c875c6793540ec9bec8
prasetion <prasetion@gmail.com>
2025-06-28 17:23:26 +0700

    delete place and fetch initial data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import {useRef, useState, useCallback} from 'react';
+import {useRef, useState, useCallback, useEffect} from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import {updateUserPlaces} from "./http.js";
+import {fetchUserPlaces, updateUserPlaces} from "./http.js";
 import Error from "./components/Error.jsx";
 
 function App() {
     const selectedPlace = useRef();
-
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState()
     const [userPlaces, setUserPlaces] = useState([]);
     const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState()
     const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    useEffect(() => {
+        async function fetchPlaces() {
+            setIsFetching(true)
+            try {
+                const places = await fetchUserPlaces();
+                setUserPlaces(places);
+            } catch (e) {
+                setError({message: e.message || "error get user places"})
+            }
+            setIsFetching(false);
+        }
+
+        fetchPlaces();
+    }, [])
 
     function handleStartRemovePlace(place) {
         setModalIsOpen(true);
@@ -48,6 +64,15 @@ function App() {
             prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
         );
 
+        try {
+            const deletePlace = await updateUserPlaces(userPlaces.filter((place) => place.id !== selectedPlace.current.id));
+        } catch (e) {
+            setUserPlaces(userPlaces);
+            setErrorUpdatingPlaces({
+                message: e.message || "failed to delete place"
+            })
+        }
+
         setModalIsOpen(false);
     }, []);
 
@@ -78,13 +103,16 @@ function App() {
                 </p>
             </header>
             <main>
-                <Places
+                {error && <Error title="Error occured" message={error.message}/>}
+                {!error && <Places
                     title="I'd like to visit ..."
                     fallbackText="Select the places you would like to visit below."
+                    isLoading={isFetching}
+                    loadingText="Loading places..."
                     places={userPlaces}
                     onSelectPlace={handleStartRemovePlace}
                 />
-
+                }
                 <AvailablePlaces onSelectPlace={handleSelectPlace}/>
             </main>
         </>
--- src/http.js
+++ src/http.js
@@ -7,8 +7,17 @@ export async function fetchAvailablePlaces() {
     return data.places;
 }
 
+export async function fetchUserPlaces() {
+    const res = await fetch('http://localhost:3000/user-places');  // Changed from /places to /user-places
+    const data = await res.json();
+
+    if (!res.ok)
+        throw new Error("failed to get user places");
+    return data.places;
+}
+
 export async function updateUserPlaces(places) {
-    const response = fetch('http://localhost:3000/user-placess', {
+    const response = await fetch('http://localhost:3000/user-places', {
         method: 'PUT',
         body: JSON.stringify({places}),
         headers: {
@@ -17,7 +26,7 @@ export async function updateUserPlaces(places) {
     });
 
     const resData = await response.json();
-    if (!resData.ok)
+    if (!response.ok)
         throw new Error("failed to update user places");
 
     return resData.message;
