f0294263ad6c490fa7ce43d1c51d7e5b9181c164
prasetion <prasetion@gmail.com>
2025-06-28 16:44:29 +0700

    update user data using PUT

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 import Error from "./Error.jsx";
 import {sortPlacesByDistance} from "../loc.js";
+import {fetchAvailablePlaces} from "../http.js";
 
 export default function AvailablePlaces({onSelectPlace}) {
     const [availablePlaces, setAvailablePlaces] = useState([])
@@ -13,7 +14,7 @@ export default function AvailablePlaces({onSelectPlace}) {
         async function fetchPlaces() {
             setIsLoading(true);
             try {
-                const places = await fetchPlaces();
+                const places = await fetchAvailablePlaces();
                 navigator.geolocation.getCurrentPosition((pos) => {
                     const sortedPlaces = sortPlacesByDistance(places, pos.coords.latitude, pos.coords.longitude);
                     setAvailablePlaces(sortedPlaces);
--- src/http.js
+++ src/http.js
@@ -1,8 +1,24 @@
-export async function fetchPlaces() {
+export async function fetchAvailablePlaces() {
     const res = await fetch('http://localhost:3000/places');
     const data = await res.json();
 
     if (!res.ok)
         throw new Error("failed to get places");
-    return data;
+    return data.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = fetch('http://localhost:3000/user-placess', {
+        method: 'PUT',
+        body: JSON.stringify({places}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+    if (!resData.ok)
+        throw new Error("failed to update user places");
+
+    return resData.message;
 }
